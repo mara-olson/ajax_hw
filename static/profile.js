ea8778bab9ddd1bf5f18c9e0bda463323c95559a
mara-olson <mara.c.olson@gmail.com>
2022-04-11 21:56:46 -0700

    Form submission data saved & displayed to user

--- static/profile.js
+++ static/profile.js
@@ -2,13 +2,29 @@ function submitProfile(evt) {
   evt.preventDefault();
 
   const data = {
-    name: document.querySelector('#name-field').value,
-    // fill in the rest
+    name: document.querySelector("#name-field").value,
+    age: document.querySelector("#age-field").value,
+    occupation: document.querySelector("#occupation-field").value,
   };
 
-  // make request to server to get the data
-  // add the data the server returns to the document
-  // (you can add it to the end of the div with ID "profiles")
+  fetch("/api/profile", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((jsonData) => {
+      document
+        .querySelector("#profiles")
+        .insertAdjacentHTML(
+          "beforeend",
+          ` <li>${jsonData.fullname} the ${jsonData.occupation} is ${jsonData.age}</li>`
+        );
+    });
 }
 
-document.querySelector('#profile-form').addEventListener('submit', submitProfile);
+document
+  .querySelector("#profile-form")
+  .addEventListener("submit", submitProfile);
